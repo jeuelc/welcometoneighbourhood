69928bc2bc320d8cc333adfccd8218289e7d8209
Jeuel Condahan <jeuelc@yahoo.com>
2016-09-05 19:05:28 +0800

    background change

--- wp-content/themes/wttn/js/custom-wttn-js.php
+++ wp-content/themes/wttn/js/custom-wttn-js.php
@@ -17,5 +17,34 @@
             }*/
         });
         $(".banner-1-content").find("img").css("margin-top", bannerContentMarginTop);
+        
+        var images = Array("<?php bloginfo('template_directory');?>/images/header-background.jpg",
+               "<?php bloginfo('template_directory');?>/images/header-background2.jpg",
+               "<?php bloginfo('template_directory');?>/images/header-background3.jpg");
+var currimg = 0;
+
+
+    function loadimg(){
+
+       $('#content-1').animate({ opacity: 1 }, 500,function(){
+            //finished animating, minifade out and fade new back in           
+            $('#content-1').animate({ opacity: 0.7 }, 100,function(){
+                currimg++;
+                if(currimg > images.length-1){
+                    currimg=0;
+                }
+                var newimage = images[currimg];
+
+                $('#content-1').css("background-image", "url("+newimage+")"); 
+                $('#content-1').animate({ opacity: 1 }, 200,function(){
+                    setTimeout(loadimg,20000);
+                });
+
+            });
+
+        });
+
+    }
+    setTimeout(loadimg,20000);
     });
 </script>

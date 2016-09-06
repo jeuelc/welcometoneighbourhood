209c59999796afd2dd8acc034052b5c51582b62e
Jeuel Condahan <jeuelc@yahoo.com>
2016-09-06 09:56:49 +0800

    change execution of scripts after all images are loaded

--- wp-content/themes/wttn/js/custom-wttn-js.php
+++ wp-content/themes/wttn/js/custom-wttn-js.php
@@ -1,5 +1,5 @@
 <script>
-    $(document).ready(function(){
+    $(window).on("load", function(){
         var windowHeight = $(window).innerHeight();
         var windowWidth = $(window).innerWidth();
         var bannerContentHeight = $(".banner-1-content").outerHeight();
@@ -21,30 +21,28 @@
         var images = Array("<?php bloginfo('template_directory');?>/images/header-background.jpg",
                "<?php bloginfo('template_directory');?>/images/header-background2.jpg",
                "<?php bloginfo('template_directory');?>/images/header-background3.jpg");
-var currimg = 0;
+        var currimg = 0;
 
 
-    function loadimg(){
+        function loadimg(){
+           $('#content-1').animate({ opacity: 1 }, 500,function(){
+                $('#content-1').animate({ opacity: 0.7 }, 100,function(){
+                    currimg++;
+                    if(currimg > images.length-1){
+                        currimg=0;
+                    }
+                    var newimage = images[currimg];
 
-       $('#content-1').animate({ opacity: 1 }, 500,function(){
-            //finished animating, minifade out and fade new back in           
-            $('#content-1').animate({ opacity: 0.7 }, 100,function(){
-                currimg++;
-                if(currimg > images.length-1){
-                    currimg=0;
-                }
-                var newimage = images[currimg];
+                    $('#content-1').css("background-image", "url("+newimage+")"); 
+                    $('#content-1').animate({ opacity: 1 }, 200,function(){
+                        setTimeout(loadimg,20000);
+                    });
 
-                $('#content-1').css("background-image", "url("+newimage+")"); 
-                $('#content-1').animate({ opacity: 1 }, 200,function(){
-                    setTimeout(loadimg,20000);
                 });
 
             });
 
-        });
-
-    }
-    setTimeout(loadimg,20000);
+        }
+        setTimeout(loadimg,20000);
     });
 </script>
